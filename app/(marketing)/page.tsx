c6a6cf983fdd5fc061381706be023335c825c98d
Andrew Bwogi <andrew.bwogi@gmail.com>
2024-05-26 21:27:37 -0700

    initial

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      Marketing Home page
+      {/* <Button variant="purple">Hello</Button> */}
+    </div>
+  );
+};
+
+export default HomePage;
