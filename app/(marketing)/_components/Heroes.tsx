9502b970c10f48730126fd5279b09033b992f508
Andrew Bwogi <andrew.bwogi@gmail.com>
2024-05-27 18:44:21 -0700

    marketing page starts

--- app/(marketing)/_components/Heroes.tsx
+++ app/(marketing)/_components/Heroes.tsx
@@ -1,7 +1,20 @@
 "use client";
 
+import Image from "next/image";
+
 const Heroes = () => {
-  return <div>Heroes</div>;
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <Image
+          src="/documents.png"
+          fill
+          alt="Documents"
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Heroes;
